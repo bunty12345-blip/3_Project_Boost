d6e5358b13249da8f6cfb0629fc483bc4403a68b
SANCHIT\Sanchit Sharma <sanchit.sharma99@gmail.com>
2018-05-23 07:42:44 +0530

    fixing roation bugs , movements and adding abstacle

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -16,30 +16,42 @@ public class Rocket : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        ProcessInput();
+        Rotate();
+		Thrust();
     }
-
-    private void ProcessInput()
+ private void Thrust()
     {
-        if(Input.GetKey(KeyCode.Space)){      //can thrust while rotating 
-			print("Thrusting !");
-			rigidBody.AddRelativeForce(Vector3.up);
-			if(!audioSource.isPlaying){   //so that it doesn't layer on top of each other
-			audioSource.Play();
-			}
-		}
-		else{
-				audioSource.Stop();
-			}
-
-		if(Input.GetKey(KeyCode.A)){
-			transform.Rotate(Vector3.forward);
-			print("Rotating Left !");
-		}
-
-		if(Input.GetKey(KeyCode.D)){
-			transform.Rotate(-Vector3.forward);
-			print("Rotating Right !");
-		}
+        if (Input.GetKey(KeyCode.Space))
+        {      //can thrust while rotating 
+            print("Thrusting !");
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {   //so that it doesn't layer on top of each other
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
     }
+    private void Rotate()
+    {
+		rigidBody.freezeRotation = true ;  // take manual control of rotation 
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+            print("Rotating Left !");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+            print("Rotating Right !");
+        }
+
+		rigidBody.freezeRotation = false ; // resume physics control of rotation . 
+    }
+
+   
 }
