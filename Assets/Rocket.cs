a6336b410033476b7f83b2053a5a0ce2a49a2c37
SANCHIT\Sanchit Sharma <sanchit.sharma99@gmail.com>
2018-05-26 06:55:27 +0530

    lecture 54 , 55 , 56 building and connecting level & invoke() function to delay level loading

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
                     Rigidbody rigidBody;
 					AudioSource audioSource;
                     [SerializeField] float rcsThrust = 100f;
                     [SerializeField] float mainThrust = 100f;
+
+                    enum State {Alive ,Trancending , Dying};
+                    State state = State.Alive;
 
 	// Use this for initialization
 	void Start () {
@@ -17,20 +21,43 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update ()
-    {
+    {   if( state== State.Alive){
         Rotate();
 		Thrust();
     }
+    }
     void OnCollisionEnter(Collision collision){
+
+        if(state !=State.Alive){
+            return;
+        }
+
         switch (collision.gameObject.tag){
             case "Friendly"  : //do nothing 
             break;
-            default : print("Dead");
+            case "Finish":
+            state = State.Trancending;
+               Invoke("LoadNextScene",1f);
+                break;
+
+            default : 
+            state  = State.Dying;
+            Invoke("LoadFirstLevel",1f);
             //kill player
             break;
         }
     }
- private void Thrust()
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+     private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {      //can thrust while rotating 
